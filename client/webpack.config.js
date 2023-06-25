084b4e38ec783284e8197028a6b3c89d34e872a3
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-25 16:23:43 -0700

    added css loaders an babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,9 +3,7 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
+//Add and configure workbox plugins for a service worker and manifest file.
 module.exports = () => {
   return {
     mode: 'development',
@@ -43,14 +41,28 @@ module.exports = () => {
             src: path.resolve('src/images/logo.png'),
             sizes: [96, 128, 192, 256, 384, 512],
             destination: path.join('assets', 'icons')
-          }
-        ]
-      })
+          },
+        ],
+      }),
     ],
-
+//Add CSS loaders and babel to webpack.
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        }, 
       ],
     },
   };
