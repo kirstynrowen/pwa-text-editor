a1dd618d17056edec1e7e85afd4448e53a39398c
kirstynrowen <50176404+kirstynrowen@users.noreply.github.com>
2023-06-25 17:12:11 -0700

    working on getDb method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -25,7 +25,7 @@ export const putDb = async (content) => {
   const request = store.put({id: 1, value: content});
   // Get confirmation of request
   const result = await request;
-  
+
   console.log('Data successfully saved to dataabse', result.value);
 };
 
@@ -33,7 +33,22 @@ export const putDb = async (content) => {
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
 
+  // Create a connection to the database database and version we want to use.
+  const contactDb = await openDB('jate', 1);
+
+  // Create a new transaction and specify the database and data privileges.
+  const tx = contactDb.transaction('jate', 'readonly');
+
+  // Open up the desired object store.
+  const store = tx.objectStore('jate');
+
+  // Use the .getAll() method to get all data in the database.
+  const request = store.getAll();
+
+  // Get confirmation of the request.
+  const result = await request;
+  console.log('result.value', result);
+  return result;
 };
-console.error('getDb not implemented');
 
 initdb();
